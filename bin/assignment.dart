c0543ecdf0be1b9f80b936deef70063422e3e3bb
ibshar <cse_182210012101018@lus.ac.bd>
2024-06-17 01:43:19 +0600

    changes

--- bin/assignment.dart
+++ bin/assignment.dart
@@ -1,5 +1,4 @@
 
-
 class Car {
   String brand;
   String model;
@@ -8,24 +7,21 @@ class Car {
 
   static int cars = 0;
 
-  Car(this.brand,
-      this.model,
-      this.year,) {
-      cars++;
+  Car(
+    this.brand,
+    this.model,
+    this.year,
+  ) {
+    cars++;
   }
-
-
-
 
   void drive(double miles) {
     milesDriven = miles;
   }
 
-
-  double getMilesdriven() {
-    return milesDriven;
+  void getMilesdriven() {
+    print(' car total run for $milesDriven miles ');
   }
-
 
   void getBrand() {
     print(" brand name of the car is $brand");
@@ -41,38 +37,34 @@ class Car {
 
   void getAge() {
     int current = DateTime.now().year;
-    int age = current-year;
+    int age = current - year;
     print(" age of the car is $age");
-
   }
 }
 
-
-  void main() {
-    print("\n");
-    Car volkswagen = Car("volkswagen", "beetly", 1965);
-    volkswagen.milesDriven = 100;
-    volkswagen.getBrand();
-    volkswagen.getModel();
-    volkswagen.getMilesdriven();
-    volkswagen.getYear();
-    volkswagen.getAge();
-    print("\n");
-    Car toyota = Car("Toyota", 'lc', 2020);
-    toyota.milesDriven=20;
-    toyota.getBrand();
-    toyota.getModel();
-    toyota.getMilesdriven();
-    toyota.getYear();
-    toyota.getAge();
-    print("\n");
-    Car honda = Car('Honda', 'civic', 2018);
-    honda.milesDriven=409;
-    honda.getBrand();
-    honda.getModel();
-    honda.getMilesdriven();
-    honda.getYear();
-    honda.getAge();
-  }
-
-
+void main() {
+  print("\n");
+  Car volkswagen = Car("volkswagen", "beetly", 1965);
+  volkswagen.milesDriven = 100;
+  volkswagen.getBrand();
+  volkswagen.getModel();
+  volkswagen.getMilesdriven();
+  volkswagen.getYear();
+  volkswagen.getAge();
+  print("\n");
+  Car toyota = Car("Toyota", 'lc', 2020);
+  toyota.milesDriven = 20;
+  toyota.getBrand();
+  toyota.getModel();
+  toyota.getMilesdriven();
+  toyota.getYear();
+  toyota.getAge();
+  print("\n");
+  Car honda = Car('Honda', 'civic', 2018);
+  honda.milesDriven = 409;
+  honda.getBrand();
+  honda.getModel();
+  honda.getMilesdriven();
+  honda.getYear();
+  honda.getAge();
+}
